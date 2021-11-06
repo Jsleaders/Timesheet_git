1276d5d3dd25e0d4828329a676c45a0828486b9b
youssefmimouni08 <youssef.mimouni@esprit.tn>
2021-11-06 19:58:28 +0100

    editing script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 set -x
 MYSQL_PASSWORD=zzz
 service mysql start
-mkdir /log
+mkdir -p D:/logs/
 sleep 5s 
 mysql -uroot -p$MYSQL_PASSWORD -e "CREATE USER 'youssef'@'localhost' IDENTIFIED BY 'password';GRANT ALL PRIVILEGES ON *.* TO 'youssef'@'localhost' WITH GRANT OPTION;"
 mysql -uroot -p$MYSQL_PASSWORD -e "CREATE USER 'youssef'@'%' IDENTIFIED BY 'password';GRANT ALL PRIVILEGES ON *.* TO 'youssef'@'%' WITH GRANT OPTION;"
